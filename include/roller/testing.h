9bc7c538b06b31420570b099f109db64847e6cc6
Mehul <mehtomar@gmail.com>
2024-10-31 14:15:44 -0400

    Removed useless files

--- include/roller/testing.h
+++
@@ -1,7 +0,0 @@
-#ifndef _ROLL_
-#define _ROLL_
-
-void testFunc(void);
-
-#endif
-
--- src/test.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include <roller/testing.h>
-
-void testFunc(void) {
-    std::cout << "This compiles and works fine" << std::endl;
-}
-
